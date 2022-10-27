4f0c5cd54fbf926c0010be7298db54657f799bbe
unknown <2889067003@qq.com>
2022-10-28 01:45:57 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__, static_folder='style')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/tool')
+def tool():
+    return render_template('tool.html')
+
+
+@app.route('/study')
+def study():
+    return render_template('study.html')
+
+
+@app.route('/more')
+def more():
+    return render_template('more.html')
